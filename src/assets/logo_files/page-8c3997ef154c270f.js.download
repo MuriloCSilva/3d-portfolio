6d28949c5ae32fba89034efaed006195482a3296
Murilo Câmara da Silva <murilocs.sjc@gmail.com>
2024-02-13 20:22:41 -0300

    first commit

---
+++ src/assets/logo_files/page-8c3997ef154c270f.js.download
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[970],{1299:function(e,n,s){Promise.resolve().then(s.t.bind(s,5164,23)),Promise.resolve().then(s.t.bind(s,8328,23))}},function(e){e.O(0,[164,328,628,935,744],function(){return e(e.s=1299)}),_N_E=e.O()}]);
---
+++ src/components/Tech.jsx
@@ -0,0 +1,18 @@
+import { technologies } from "../constants"
+import { SectionWrapper } from "../hoc"
+import { BallCanvas } from "./canvas"
+
+
+const Tech = () => {
+  return (
+    <div className="flex flex-row flex-wrap justify-center gap-10">
+      {technologies.map((technology) => (
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default SectionWrapper(Tech, "");
